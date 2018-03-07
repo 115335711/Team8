bad8729a361f3dcb393500e304eb4638706f1a77
115335711 <35601441+115335711@users.noreply.github.com>
2018-03-07 02:48:31 +0000

    Add files via upload

--- www3/multi_player.php
+++ www3/multi_player.php
@@ -3,7 +3,12 @@ include 'basic.php';
 HTML_start('Multi-Player Version');
 if (isset($_SESSION['user_name'])) {
 	//if the user is logged in, display the contents of the page
-	createCanvasAndChat();
+	//createCanvasAndChat();
+	print('<br><br><br>'."\n");
+	print('<p>To play the multi-player version, please <a href="/game/game.zip" download>download</a> the ZIP folder containing the game files. After downloading the folder, please unzip it and follow the instructions in the README.txt file to set up and play the game.</p>'."\n");
+	print('<br><br><br>'."\n");
+	print('<p>A permanent Internet connection is required to play this game as well as a knowledge of the host&#39;s IP address.</p>'."\n");
+	print('<p>We do not recommend messing with the .py files and we are not responsible for what may happen if you do so.</p>');
 }
 HTML_end();
 if (!isset($_SESSION['user_name'])) {
